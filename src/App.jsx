fecaddcb0820b8be2a712343dbc3f6db8e640ecd
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-11-07 15:43:45 +0300

    clear

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import "./App.css";
-import Header from "./components/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
