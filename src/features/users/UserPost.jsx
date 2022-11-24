c7d0a9b26b38a681ddb37b967ba8197d68182793
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-11-24 23:34:00 +0300

    update

--- src/features/users/UserPost.jsx
+++ src/features/users/UserPost.jsx
@@ -1,19 +1,39 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useGetPostsQuery } from "../posts/postSlice";
+import { useGetUsersQuery } from "./usersSlice";
 
 function UserPost() {
   const { userid } = useParams();
-  const { data } = useGetPostsQuery("getUsers", {
+  const { data: posts } = useGetPostsQuery("getUsers", {
     selectFromResult: ({ data }) => ({
       data: data?.filter((users) => Number(users?.userid) === Number(userid)),
     }),
   });
-  console.log(data);
+  const { data: users } = useGetUsersQuery("getUsers", {
+    selectFromResult: ({ data }) => ({
+      data: data?.find((user) => user.id === Number(userid)),
+    }),
+  });
+
+  const postList = posts?.map((user, i) => {
+    return (
+      <li>
+        <Link className="underline text-xl font-medium">
+          {i + 1}. {user.title}
+        </Link>
+      </li>
+    );
+  });
   return (
-    <>
-      <ul>yakında</ul>
-    </>
+    <div className="flex flex-col items-center mt-5">
+      <h1 className="text-black text-2xl font-extrabold">
+        {users?.name} Post List
+      </h1>
+      <ul className="mt-3 flex flex-col">
+        {postList} {!posts.length && <div className="text-xl">No Post!</div>}
+      </ul>
+    </div>
   );
 }
 
