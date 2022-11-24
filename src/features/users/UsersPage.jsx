503ead4a8489b326dd60742534a96bfd7a85805d
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-11-24 12:05:08 +0300

    hoc

--- src/features/users/UsersPage.jsx
+++ src/features/users/UsersPage.jsx
@@ -3,19 +3,18 @@ import { useGetUsersQuery } from "./usersSlice";
 
 function UsersPage() {
   const { data: users, isError, isLoading } = useGetUsersQuery();
+  const usersContent = users?.map((user, i) => (
+    <li className="mb-2 text-xl text-black font-extrabold">
+      {i + 1}. {user?.name}
+    </li>
+  ));
 
   if (isLoading) return <div>Loading Users List....</div>;
   if (isError) return <div>Errors...</div>;
 
   return (
     <div className="w-full flex justify-center mt-5">
-      <ul className="flex flex-col items-start">
-        {users?.map((user, i) => (
-          <li className="mb-2 text-xl text-black font-extrabold">
-            {i + 1}. {user?.name}
-          </li>
-        ))}
-      </ul>
+      <ul className="flex flex-col items-start">{usersContent}</ul>
     </div>
   );
 }
