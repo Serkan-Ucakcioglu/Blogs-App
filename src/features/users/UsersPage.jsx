4a8874bc257669b0307be35e31d4e1f2c3608d5a
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-11-24 12:01:28 +0300

    users page user list add

--- src/features/users/UsersPage.jsx
+++ src/features/users/UsersPage.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useGetUsersQuery } from "./usersSlice";
 
 function UsersPage() {
-  return <div className="w-full flex justify-center">users</div>;
+  const { data: users, isError, isLoading } = useGetUsersQuery();
+
+  if (isLoading) return <div>Loading Users List....</div>;
+  if (isError) return <div>Errors...</div>;
+
+  return (
+    <div className="w-full flex justify-center mt-5">
+      <ul>
+        {users?.map((user, i) => (
+          <li className="mb-2 text-xl text-black font-extrabold">
+            {i + 1}. {user?.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default UsersPage;
