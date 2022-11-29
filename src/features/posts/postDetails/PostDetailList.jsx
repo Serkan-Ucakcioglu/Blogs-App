59a5dc6747f5c819d052e72dc5aedcb48a2fda9c
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-11-29 16:36:29 +0300

    font-weight and font-size and gap add

--- src/features/posts/postDetails/PostDetailList.jsx
+++ src/features/posts/postDetails/PostDetailList.jsx
@@ -3,10 +3,10 @@ import EmojiList from "../../Emoji/EmojiList";
 function PostDetailList({ post, users }) {
   const content = (
     <div
-      className="flex flex-col mt-3 justify-center items-center w-80 h-48 bg-gray-500 font-extrabold text-white rounded"
+      className="flex flex-col mt-3 p-2 gap-2 justify-center items-center w-80 h-48 bg-gray-500 font-extrabold text-white rounded"
       key={post?.id}
     >
-      <h1>{post?.title}</h1>
+      <h1 className="font-extrabold text-xl mb-4">{post?.title}</h1>
       <div className="flex flex-row">👥 - {users?.name}</div>
       <span>{post?.body}</span>
       <span>🕑 {post?.date.substring(0, 10)}</span>
