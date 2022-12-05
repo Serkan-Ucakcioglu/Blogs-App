50a31f60a82a8de577edb86ceee280accd4381cd
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-12-05 14:11:57 +0300

    post detail page update

--- src/features/posts/postDetails/PostDetailList.jsx
+++ src/features/posts/postDetails/PostDetailList.jsx
@@ -3,13 +3,13 @@ import EmojiList from "../../Emoji/EmojiList";
 function PostDetailList({ post, users }) {
   const content = (
     <div
-      className="flex flex-col mt-3 p-2 gap-2 justify-center items-center w-80 h-48 bg-gray-500 font-extrabold text-white rounded"
+      className="flex flex-col mt-3 p-2 gap-2 justify-center items-center w-80 h-48 bg-purple-700 text-white rounded"
       key={post?.id}
     >
-      <h1 className="font-extrabold text-xl mb-4">{post?.title}</h1>
+      <h1 className="font-extrabold text-2xl">{post?.title}</h1>
       <div className="flex flex-row">👥 - {users?.name}</div>
-      <span>{post?.body}</span>
-      <span>🕑 {post?.date.substring(0, 10)}</span>
+      <p>{post?.body}</p>
+      <p>🕑 {post?.date.substring(0, 10)}</p>
       <EmojiList post={post} />
     </div>
   );
